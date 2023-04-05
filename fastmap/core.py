70c58c3b79d534e02b988c3cf6910309ee6b6416
Malcolm White <malcolmw@mit.edu>
2023-04-05 14:05:46 -0400

    Debug GPU implementation.

--- fastmap/core.py
+++ fastmap/core.py
@@ -8,16 +8,8 @@ Created on Thu Feb 16 14:24:10 2023
 
 print('Importing fastmap.core')
 
-# _xp_ is the array module to use for batch distance calculations.
-# It can be either numpy or cupy. The default is numpy.
-global _xp_
-global get_array
-
 import numpy as np
 import tqdm
-
-_xp_ = np
-get_array = lambda array: array
 
 DEFAULT_BATCH_SIZE = 1024
 EPSILON = 1e-9
@@ -25,10 +17,11 @@ EPSILON = 1e-9
 class FastMapABC:
 
     def __init__(
-        self, 
+        self,
         n_dim,
         show_progress=False,
-        batch_size=DEFAULT_BATCH_SIZE
+        batch_size=DEFAULT_BATCH_SIZE,
+        cupy=False
     ):
         '''
         Implements the FastMap algorithm.
@@ -41,6 +34,8 @@ class FastMapABC:
             Path to store model.
         show_progress: bool, optional
             Show TQDM progress bar. The default is False.
+        cupy: bool, optional
+            Use cupy backend. The default is False.
 
         Returns
         -------
@@ -51,8 +46,12 @@ class FastMapABC:
         self._n_dim = n_dim
         self._batch_size = batch_size
         self.show_progress = show_progress
-        
-        
+        if cupy is False:
+            self.numpy()
+        else:
+            self.cupy()
+
+
     @property
     def batch_size(self):
         '''
@@ -69,8 +68,8 @@ class FastMapABC:
         if not isinstance(value, int):
             raise TypeError('batch_size must be an int.')
         self._batch_size = value
-        
-        
+
+
     @property
     def n_dim(self):
         '''
@@ -81,8 +80,8 @@ class FastMapABC:
 
         '''
         return self._n_dim
-    
-    
+
+
     @property
     def n_obj(self):
         '''
@@ -93,7 +92,7 @@ class FastMapABC:
 
         '''
         return len(self.X)
-    
+
     @property
     def pivot_ids(self):
         '''
@@ -110,18 +109,18 @@ class FastMapABC:
                 dtype=np.uint16,
             )
         return self._pivot_ids
-    
-    
+
+
     @property
     def show_progress(self):
         return self._show_progress
-    
+
     @show_progress.setter
     def show_progress(self, value):
         if value not in (True, False):
             raise(ValueError('show_progress must be either True or False.'))
         self._show_progress = value
-    
+
     @property
     def supervised(self):
         '''
@@ -132,8 +131,8 @@ class FastMapABC:
 
         '''
         return self._supervised
-    
-    
+
+
     @property
     def W_piv(self):
         if not hasattr(self, '_W_piv'):
@@ -144,7 +143,7 @@ class FastMapABC:
             )
         return self._W_piv
 
-    
+
     @property
     def X(self):
         '''
@@ -159,8 +158,8 @@ class FastMapABC:
     @X.setter
     def X(self, value):
         self._X = value
-        
-        
+
+
     @property
     def X_piv(self):
         if not hasattr(self, '_X_piv'):
@@ -170,8 +169,8 @@ class FastMapABC:
                 dtype=self.X.dtype
             )
         return self._X_piv
-        
-        
+
+
     @property
     def y(self):
         '''
@@ -190,8 +189,8 @@ class FastMapABC:
             self._supervised = True
         else:
             self._supervised = False
-        
-        
+
+
     def _choose_pivots(self, n_proc=None):
         '''
         A heuristic algorithm to choose distant pivot objects adapted
@@ -224,8 +223,8 @@ class FastMapABC:
                 break
 
         furthest = self.furthest(
-            j_obj, 
-            label=0 if self.supervised else None, 
+            j_obj,
+            label=0 if self.supervised else None,
             n_proc=n_proc
         )
         for i_obj in furthest:
@@ -233,8 +232,8 @@ class FastMapABC:
                 break
 
         furthest = self.furthest(
-            i_obj, 
-            label=1 if self.supervised else None, 
+            i_obj,
+            label=1 if self.supervised else None,
             n_proc=n_proc
         )
         for j_obj in furthest:
@@ -243,33 +242,23 @@ class FastMapABC:
 
         return i_obj, j_obj
 
-        
-        
+
+
+    def numpy(self):
+        self.xp = np
+        self.get_array = lambda array: array
+
     def cupy(self):
-        '''
-        Use the cupy module to accelerate array calculations using GPU.
-
-        Returns
-        -------
-        None.
-
-        '''
-        global _xp_
-        global get_array
-        try:
-            import cupy as _xp_
-            get_array = lambda array: array.get()
-        except ModuleNotFoundError:
-            print('CuPy module is not installed. Falling back to NumPy.')
-
-        
+        import cupy as xp
+        self.xp = xp
+        self.get_array = lambda array: array.get() if isinstance(array, xp.ndarray) else array
 
     def distance_matrix(
-        self, 
-        i_objs, 
-        j_objs, 
-        X_1=None, 
-        X_2=None, 
+        self,
+        i_objs,
+        j_objs,
+        X_1=None,
+        X_2=None,
         W_1=None,
         W_2=None
     ):
@@ -297,23 +286,24 @@ class FastMapABC:
         if W_2 is None:
             W_2 = self.W
 
-        X_j = _xp_.array(X_2[j_objs])
-        dW  = _xp_.square(_xp_.array(W_1[i_objs] - W_2[j_objs]))
+        X_j = self.xp.array(X_2[j_objs])
+        dW  = self.xp.square(self.xp.array(W_1[i_objs]) - self.xp.array(W_2[j_objs]))
 
-        dist = _xp_.concatenate([
+        dist = [
             self._distance_func(
-                X_1[i_objs[i: i+self.batch_size]],
-                X_j
+                self.xp.array(X_1[i_objs[i: i+self.batch_size]]),
+                self.xp.array(X_j)
             )
             for i in range(0, len(i_objs), self.batch_size)
-        ])
+        ]
+        dist = self.xp.concatenate(dist) if len(dist) > 1 else dist[0]
 
         for i in range(self._ihyprpln):
-            dist = _xp_.sqrt(_xp_.clip(dist**2 - dW[:, i], 0,  _xp_.inf))
+            dist = self.xp.sqrt(self.xp.clip(dist**2 - dW[:, i], 0,  self.xp.inf))
 
         return dist
-            
-            
+
+
     def furthest(self, i_obj, label=None, n_proc=None):
         """
         Return the index of the object furthest from object with index
@@ -325,22 +315,22 @@ class FastMapABC:
         else:
             idxs = np.argwhere(self.y == label).flatten()
 
-        dW = _xp_.square(_xp_.array(self.W[idxs] - self.W[[i_obj]]))
+        dW = self.xp.square(self.xp.array(self.W[idxs] - self.W[[i_obj]]))
         dist = self._distance_func(
-            _xp_.array(self.X[idxs]),
-            _xp_.array(self.X[[i_obj]])
+            self.xp.array(self.X[idxs]),
+            self.xp.array(self.X[[i_obj]])
         )
         for i in range(self._ihyprpln):
-            dist = _xp_.sqrt(_xp_.clip(dist**2 - dW[:, i], 0, _xp_.inf))
-        
-        idxs = idxs[get_array(_xp_.argsort(dist))]
+            dist = self.xp.sqrt(self.xp.clip(dist**2 - dW[:, i], 0, self.xp.inf))
+
+        idxs = idxs[self.get_array(self.xp.argsort(dist))]
         return idxs[-1::-1]
 
 
     def fit(
-        self, 
-        X, 
-        y=None, 
+        self,
+        X,
+        y=None,
         n_proc=None
     ):
         '''
@@ -364,36 +354,36 @@ class FastMapABC:
         '''
         self.X = X
         self.y = y
-        
+
         self.W = np.full(
-            (self.n_obj, self.n_dim), 
+            (self.n_obj, self.n_dim),
             np.nan,
             dtype=np.float32
         )
-            
+
         wrapper = tqdm.tqdm if self.show_progress is True else lambda x: x
         for self._ihyprpln in wrapper(range(self.n_dim)):
             i_piv, j_piv = self._choose_pivots(n_proc=n_proc)
             self.pivot_ids[self._ihyprpln] = [i_piv, j_piv]
-            self.X_piv[self._ihyprpln, 0] = self.X[i_piv]
-            self.X_piv[self._ihyprpln, 1] = self.X[j_piv]
-            
+            self.X_piv[self._ihyprpln, 0] = self.get_array(self.X[i_piv])
+            self.X_piv[self._ihyprpln, 1] = self.get_array(self.X[j_piv])
+
             d_ij = self.distance_matrix([i_piv], [j_piv])
-            d  = _xp_.square(self.distance_matrix(np.arange(self.n_obj), i_piv))
-            d -= _xp_.square(self.distance_matrix(np.arange(self.n_obj), j_piv))
+            d  = self.xp.square(self.distance_matrix(np.arange(self.n_obj), i_piv))
+            d -= self.xp.square(self.distance_matrix(np.arange(self.n_obj), j_piv))
             # d = d.get()
             d += d_ij ** 2
             ####### Avoid divide by zero.
             d /= (2 * d_ij + EPSILON)
             #### Hack for negative distances.
-            d = _xp_.clip(d, 0, _xp_.inf)
+            d = self.xp.clip(d, 0, self.xp.inf)
             ####
-            self.W[:, self._ihyprpln] = get_array(d)
+            self.W[:, self._ihyprpln] = self.get_array(d)
 
         for i_dim, (i_piv, j_piv) in enumerate(self.pivot_ids):
             self.W_piv[i_dim, 0] = self.W[i_piv]
             self.W_piv[i_dim, 1] = self.W[j_piv]
-            
+
         del(self._pivot_ids, self._X, self.W)
         if hasattr(self, '_y'):
             del(self._y)
@@ -409,41 +399,41 @@ class FastMapABC:
 
         n_obj = len(X)
 
-        W = _xp_.zeros((n_obj, self.n_dim), dtype=(_xp_).float32)
-        X_piv = _xp_.array(self.X_piv[:])
-        W_piv = _xp_.array(self.W_piv[:])
+        W = self.xp.zeros((n_obj, self.n_dim), dtype=(self.xp).float32)
+        X_piv = self.xp.array(self.X_piv[:])
+        W_piv = self.xp.array(self.W_piv[:])
         wrapper = tqdm.tqdm if self.show_progress is True else lambda x: x
         for i_batch, i_start in enumerate(wrapper(range(
                 0,
                 n_obj,
                 self.batch_size
         ))):
-            X_batch = _xp_.array(X[i_start: i_start+self.batch_size])
+            X_batch = self.xp.array(X[i_start: i_start+self.batch_size])
             W_batch = W[i_start: i_start+self.batch_size]
             d_ij0 = self._distance_func(X_piv[:, [0]], X_piv[:, [1]])
             d_k0 = self._distance_func(
-                X_batch[:, _xp_.newaxis, _xp_.newaxis],
-                X_piv[_xp_.newaxis]
+                X_batch[:, self.xp.newaxis, self.xp.newaxis],
+                X_piv[self.xp.newaxis]
             )
             for self._ihyprpln in range(self.n_dim):
-                dW_ij = _xp_.square(W_piv[self._ihyprpln, [0]] - W_piv[self._ihyprpln, 1])
-                dW_ik = _xp_.square(W_batch - W_piv[self._ihyprpln, 0])
-                dW_jk = _xp_.square(W_batch - W_piv[self._ihyprpln, 1])
+                dW_ij = self.xp.square(W_piv[self._ihyprpln, [0]] - W_piv[self._ihyprpln, 1])
+                dW_ik = self.xp.square(W_batch - W_piv[self._ihyprpln, 0])
+                dW_jk = self.xp.square(W_batch - W_piv[self._ihyprpln, 1])
                 d_ij = d_ij0[self._ihyprpln].copy()
                 d_ik = d_k0[:, self._ihyprpln, 0].copy()
                 d_jk = d_k0[:, self._ihyprpln, 1].copy()
                 for i in range(self._ihyprpln):
-                    d_ij = _xp_.sqrt(_xp_.clip(d_ij**2 - dW_ij[:, i], 0, _xp_.inf))
-                    d_ik = _xp_.sqrt(_xp_.clip(d_ik**2 - dW_ik[:, i], 0, _xp_.inf))
-                    d_jk = _xp_.sqrt(_xp_.clip(d_jk**2 - dW_jk[:, i], 0, _xp_.inf))
-                W_batch[:, self._ihyprpln]  = _xp_.square(d_ik)
-                W_batch[:, self._ihyprpln] += _xp_.square(d_ij)
-                W_batch[:, self._ihyprpln] -= _xp_.square(d_jk)
+                    d_ij = self.xp.sqrt(self.xp.clip(d_ij**2 - dW_ij[:, i], 0, self.xp.inf))
+                    d_ik = self.xp.sqrt(self.xp.clip(d_ik**2 - dW_ik[:, i], 0, self.xp.inf))
+                    d_jk = self.xp.sqrt(self.xp.clip(d_jk**2 - dW_jk[:, i], 0, self.xp.inf))
+                W_batch[:, self._ihyprpln]  = self.xp.square(d_ik)
+                W_batch[:, self._ihyprpln] += self.xp.square(d_ij)
+                W_batch[:, self._ihyprpln] -= self.xp.square(d_jk)
                 W_batch[:, self._ihyprpln] /= (d_ij * 2 + EPSILON)
                 W[i_start: i_start+self.batch_size, self._ihyprpln]  =  W_batch[:, self._ihyprpln]
 
-        return get_array(W)
+        return self.get_array(W)
 
-        
+
 if __name__ == '__main__':
     pass
